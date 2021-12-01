5348709e8a0de0ff485fe4788befcdd9f578afb8
tonisco <tonyumeozulu@gmail.com>
2021-12-01 20:19:30 +0100

    about

--- pages/urls.py
+++ pages/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, about
 
-urlpatterns = [path('', index, name='index')]
+urlpatterns = [
+    path('', index, name='index'),
+    path('about', about, name='about')
+]
--- pages/views.py
+++ pages/views.py
@@ -6,6 +6,10 @@ from listing.models import Listing
 # Create your views here.
 
 def index(request):
-    listings = Listing.objects.all()
-    context = {'form': SearchForm(listing=listings), 'listings': listings[:9]}
+    listings = Listing.objects.filter(is_published=True).order_by('-list_date')
+    context = {'form': SearchForm(listing=listings), 'listings': listings[:6]}
     return render(request, 'pages/index.html', context=context)
+
+
+def about(request):
+    return render(request, 'pages/about.html')
